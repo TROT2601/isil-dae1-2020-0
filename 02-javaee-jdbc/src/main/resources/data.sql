56bc573af703c39bca7b6890cb2b86585d3dcf87
Jose Ventura <ventura.jos3@gmail.com>
2020-01-08 22:41:00 -0500

    add crud curso

---
+++ 02-javaee-jdbc/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+create table accounts (
+                          id serial,
+                          name varchar(100) not null,
+                          balance double precision not null,
+                          primary key (id)
+);
+
+insert into accounts(name, balance) values ('Edwin', 1000);
+insert into accounts(name, balance) values ('Jeremy', 0);
+
+create function transfer(
+    idFrom INTEGER,
+    idTo INTEGER,
+    amount double precision)
+    returns BOOLEAN
+BEGIN
+
+UPDATE accounts
+SET balance = balance - amount
+WHERE id = idFrom;
+
+UPDATE accounts
+SET balance = balance + amount
+WHERE id = idTo;
+
+return true;
+END;
